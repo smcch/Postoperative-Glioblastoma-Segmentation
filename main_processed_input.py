2698e25baefff26d3ecf211a774f3d339af31d38
Santiago Cepeda <87584415+smcch@users.noreply.github.com>
2024-05-21 18:25:55 +0200

    Update main_processed_input.py

--- main_processed_input.py
+++ main_processed_input.py
@@ -1,5 +1,24 @@
 # Tumor Segmentation Predictions Using RH-GlioSeg-nnUNet with Preprocessed NifTI Files
-# Santiago Cepeda - Department of Neurosurgery - Río Hortega University Hospital
+# ==============================================================================#
+#  Author:       * Roberto Romero-Oraá and + Santiago Cepeda                    #
+#  Copyright:    * Biomedical Engineering Group                                #
+#                + Río Hortega University Hospital                             #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+# ==============================================================================#
+
+# This script is tailored for processing single-timepoint scans. It expects input files that have already undergone coregistration and skull stripping.
 
 import os
 import shutil
